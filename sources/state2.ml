01a5a5ceaa0816c3630783167e9ba108e01f00e2
lmdexpr <lmdexpr@gmail.com>
2022-12-29 21:23:59 +0900

    refactoring state2

--- sources/state2.ml
+++ sources/state2.ml
@@ -11,38 +11,34 @@ module type STATE = sig
 end
 
 module State (S : sig type t end) : STATE with type t = S.t = struct
-
   type t = S.t
 
-  type _ Effect.t += Get : t Effect.t
+  type _ Effect.t +=
+    | Get : t Effect.t
     | Put : t -> unit Effect.t
     | History : t list Effect.t
 
-  let get () = perform Get
-
-  let put v = perform (Put v)
-
-  let history () = perform History
+  let get ()     = perform Get
+  and put v      = perform (Put v)
+  and history () = perform History
 
   open struct
     let head ~default = function
       | [] -> default
       | xs -> List.hd xs
+    let identity x = x
   end
 
   let run f ~init =
-    let rec loop : type a r. t -> t list -> (a, r) continuation -> a -> r =
-      fun init state k x ->
-        continue_with k x
-        { retc = (fun result -> result);
-          exnc = (fun e -> raise e);
-          effc = (fun (type b) (eff: b Effect.t) ->
-            match eff with
-            | Get     -> Some (fun (k: (b,r) continuation) -> loop init state k @@ head ~default:init state)
-            | Put v   -> Some (fun (k: (b,r) continuation) -> loop init (v::state) k ())
-            | History -> Some (fun (k: (b,r) continuation) -> loop init state k @@ List.rev state)
-            | _       -> None)
-        }
+    let rec loop : type a r. t -> t list -> (a, r) continuation -> a -> r = fun init state k x ->
+      let effc : type b. b Effect.t -> ((b, r) continuation -> r) option =
+        function
+        | Get     -> Some (fun k -> loop init state k @@ head ~default:init state)
+        | Put v   -> Some (fun k -> loop init (v::state) k ())
+        | History -> Some (fun k -> loop init state k @@ List.rev state)
+        | _       -> None
+      in
+      continue_with k x { retc = identity ; exnc = raise ; effc }
     in
     loop init [] (fiber f) ()
 end
